b4f62f3b2e09fc1dbabc3fd53ed1a3a38421de68
giangpham <truong-giang.pham-nguyen@ant-tech.eu>
2022-10-18 20:05:55 +0700

    add netlify toml

--- src/views/admin/hien-mau.vue
+++ src/views/admin/hien-mau.vue
@@ -361,11 +361,8 @@ export default {
       this.loading = true;
       const currentDay = new Date().getDate();
       const currentMonth = new Date().getMonth() + 1;
-      console.log('🔥')
-      console.log(this.user.data.email)
       try {
         if (this.user.data.email === 'mynguyenngoc22@gmail.com') {
-          console.log('🔥checked🔥')
           const data = this.bloodstorage;
           const fileName = "hien-mau-" + currentDay + "-" + currentMonth;
           const exportType = exportFromJSON.types.xls;
