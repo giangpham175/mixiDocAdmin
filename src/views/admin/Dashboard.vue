8dbb6a451c5b6120c9175e530665a0c7edfa2f3a
giangpham <truong-giang.pham-nguyen@ant-tech.eu>
2023-01-15 01:51:12 +0700

    hpbd dip

--- src/views/admin/Dashboard.vue
+++ src/views/admin/Dashboard.vue
@@ -2,7 +2,7 @@
     <div>
         <v-container fluid>
             <v-img v-if="event" max-height="611" max-width="610"
-                src="https://media.discordapp.net/attachments/1038006225230626856/1053938621088010280/ez.png">
+                src="https://media.discordapp.net/attachments/1038006225230626856/1063889226975952996/dipdip.png?width=810&height=875">
             </v-img>
             <v-img v-else max-height="611" max-width="610"
                 src="https://media.discordapp.net/attachments/977950271663644712/1011922943497994341/2.png?width=810&height=811">
@@ -20,8 +20,8 @@ export default {
         const currentYear = (new Date()).getFullYear()
         const currentMonth = (new Date()).getMonth() + 1
         const currentDate = (new Date()).getDate()
-        if (currentYear === 2022 && currentMonth === 12) {
-            if (currentDate >= 18 && currentDate <= 19) {
+        if (currentYear === 2023 && currentMonth === 1) {
+            if (currentDate >= 15 && currentDate <= 16) {
                 this.event = true
             }
         }
